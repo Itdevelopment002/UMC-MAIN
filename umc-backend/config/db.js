0a9b39e6efaff2c0810d6267b0ce2d40095bf90f
Itdevelopment002 <itdevelopment111@gmail.com>
2025-02-14 04:03:05 -0500

    chnges_server

--- umc-backend/config/db.js
+++ umc-backend/config/db.js
@@ -1,13 +1,22 @@
 const mysql = require('mysql');
 require('dotenv').config();
 
+// const db = mysql.createConnection({
+//     host: "127.0.0.1",
+//     user: "root",
+//     password: "Root@123",
+//     // user: "root",
+//     // password: "",
+//     database: "genicminds_umc_latest_db"
+// });
+
 const db = mysql.createConnection({
     host: "127.0.0.1",
-    user: "root",
-    password: "Root@123",
+    user: "genicminds_newumcDb",
+    password: "-LMR+2cO@dN(",
     // user: "root",
     // password: "",
-    database: "genicminds_umc_latest_db"
+    database: "genicminds_newumcDb"
 });
 
 db.connect((err) => {
