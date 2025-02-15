c3f02f6b71792ff5fb176193637ad69f6556856e
Abhay31 <abhaysharma0983@gmail.com>
2025-02-15 17:31:14 +0530

    new_chngs_Abhay

--- umc-backend/routes/propertyDeptRoutes.js
+++ umc-backend/routes/propertyDeptRoutes.js
@@ -3,25 +3,35 @@ const router = express.Router();
 const db = require("../config/db.js");
 
 router.get("/property-dept", (req, res) => {
-  db.query("SELECT * FROM property_dept", (err, results) => {
+  const language = req.query.lang;
+  let query;
+  let params = [];
+  if (language) {
+    query = `SELECT * FROM property_dept WHERE language_code = ?`;
+    params.push(language);
+  } else {
+    query = "SELECT * FROM property_dept";
+  }
+
+  db.query(query, params, (err, results) => {
     if (err) throw err;
     res.json(results);
   });
 });
 
 router.post("/property-dept", (req, res) => {
-  const { description, link } = req.body;
-  const sql = "INSERT INTO property_dept (description, link) VALUES (?, ?)";
-  db.query(sql, [description, link], (err, result) => {
+  const { description, link, language_code } = req.body;
+  const sql = "INSERT INTO property_dept (description, link, language_code) VALUES (?, ?, ?)";
+  db.query(sql, [description, link, language_code], (err, result) => {
     if (err) throw err;
-    res.json({ id: result.insertId, description, link });
+    res.json({ id: result.insertId, description, link, language_code });
   });
 });
 
 router.put("/property-dept/:id", (req, res) => {
-  const { description, link } = req.body;
-  const sql = "UPDATE property_dept SET description = ?, link = ? WHERE id = ?";
-  db.query(sql, [description, link, req.params.id], (err, result) => {
+  const { description, link, language_code } = req.body;
+  const sql = "UPDATE property_dept SET description = ?, link = ?, language_code = ? WHERE id = ?";
+  db.query(sql, [description, link, language_code, req.params.id], (err, result) => {
     if (err) throw err;
     res.json({ success: true });
   });
