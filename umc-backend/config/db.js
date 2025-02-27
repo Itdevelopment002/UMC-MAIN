6c5ebd10b3cac46e6c7757da8c19624bd0c8fc36
Itdevelopment002 <itdevelopment111@gmail.com>
2025-02-27 06:28:26 -0500

    add

--- umc-backend/config/db.js
+++ umc-backend/config/db.js
@@ -1,23 +1,21 @@
 const mysql = require('mysql');
 require('dotenv').config();
 
-// const db = mysql.createConnection({
-//     host: "127.0.0.1",
-//     user: "root",
-//     password: "Root@123",
-//     // user: "root",
-//     // password: "",
-//     database: "genicminds_umc_latest_db"
-// });
-
 const db = mysql.createConnection({
     host: "127.0.0.1",
-    user: "umc_genicminds_UMCDB",
-    password: "6mHN4DfTL%Yw",
-    // user: "root",
-    // password: "",
-    database: "umc_genicminds_UMCDB"
+    user: "genicminds_newumcDb",
+    password: "caDy3}d^%R#n",
+    database: "genicminds_newumcDb"
 });
+
+// const db = mysql.createConnection({
+//     host: "127.0.0.1",
+//     user: "umc_genicminds_UMCDB",
+//     password: "6mHN4DfTL%Yw",
+//     // user: "root",
+//     // password: "",
+//     database: "umc_genicminds_UMCDB"
+// });
 
 db.connect((err) => {
     if (err) throw err;
