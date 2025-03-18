357031f937f3b1921fc593bed4261a1ae75c6033
Itdevelopment002 <itdevelopment111@gmail.com>
2025-03-18 04:22:06 -0400

    Applying .gitignore rules properly

--- umc-frontend/src/components/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const baseURL = 'https://bacumc.genicminds.com/api';
-//  export const baseURL = 'http://localhost:5011/api';
-const api = axios.create({
-  baseURL,
-});
-
-export default api;
