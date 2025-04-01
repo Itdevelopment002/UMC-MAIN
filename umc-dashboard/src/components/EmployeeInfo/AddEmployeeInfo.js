7b4d8090c87e05fdc492633f4ddf86482fc2f837
Itdevelopment002 <itdevelopment111@gmail.com>
2025-04-01 06:27:29 +0530

    chege

---
+++ umc-dashboard/src/components/EmployeeInfo/AddEmployeeInfo.js
@@ -0,0 +1,221 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import api from "../api";
+import Flatpickr from "react-flatpickr";
+import "flatpickr/dist/themes/material_blue.css";
+
+const AddEmpInfo = () => {
+  const [description, setDescription] = useState("");
+  const [link, setLink] = useState("");
+  const [language, setLanguage] = useState("");
+  const [issueDate, setIssueDate] = useState("");
+  const [errors, setErrors] = useState({});
+  const navigate = useNavigate();
+
+  const formatDate = (date) => {
+    const d = new Date(date);
+    const day = String(d.getDate()).padStart(2, "0");
+    const month = String(d.getMonth() + 1).padStart(2, "0");
+    const year = d.getFullYear();
+    return `${day}-${month}-${year}`;
+  };
+
+  const validateForm = () => {
+    const validationErrors = {};
+
+    if (!language) {
+      validationErrors.language = "Language selection is required";
+    }
+
+    if (!issueDate) {
+      validationErrors.issueDate = "Issue Date is required.";
+    }
+
+    if (!description) {
+      validationErrors.description = "Description is required.";
+    }
+
+    if (!link) {
+      validationErrors.link = "Link is required.";
+    }
+
+    setErrors(validationErrors);
+    return Object.keys(validationErrors).length === 0;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!validateForm()) {
+      return;
+    }
+
+    const formattedDate = formatDate(issueDate);
+
+    try {
+      //eslint-disable-next-line
+      const response = await api.post("/emp-info", {
+        description: description,
+        link: link,
+        language_code: language,
+        issue_date: formattedDate,
+      });
+      setDescription("");
+      setLink("");
+      setLanguage("");
+      setIssueDate("");
+      navigate("/emp-info");
+    } catch (error) {
+      console.error("Error adding emp info:", error);
+    }
+  };
+
+  return (
+    <div>
+      <div className="page-wrapper">
+        <div className="content">
+          <ol className="breadcrumb">
+            <li className="breadcrumb-item">
+              <Link to="#">Home</Link>
+            </li>
+            <li className="breadcrumb-item">
+              <Link to="/emp-info">Employee Info</Link>
+            </li>
+            <li className="breadcrumb-item active" aria-current="page">
+              Add Employee Info
+            </li>
+          </ol>
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="card-box">
+                <div className="card-block">
+                  <div className="row">
+                    <div className="col-12">
+                      <h4 className="page-title">Add Employee Info</h4>
+                    </div>
+                  </div>
+                  <form onSubmit={handleSubmit}>
+                    <div className="form-group row">
+                      <label className="col-form-label col-md-2">
+                        Select Language <span className="text-danger">*</span>
+                      </label>
+                      <div className="col-md-4">
+                        <select
+                          className={`form-control form-control-md ${errors.language ? "is-invalid" : ""
+                            }`}
+                          value={language}
+                          onChange={(e) => {
+                            setLanguage(e.target.value);
+                            if (errors.language) {
+                              setErrors({ ...errors, language: "" });
+                            }
+                          }}
+                        >
+                          <option value="">Select Language</option>
+                          <option value="en">English</option>
+                          <option value="mr">Marathi</option>
+                        </select>
+                        {errors.language && <div className="invalid-feedback">{errors.language}</div>}
+                      </div>
+                    </div>
+                    <div className="form-group row">
+                      <label className="col-form-label col-md-2">
+                        Description <span className="text-danger">*</span>
+                      </label>
+                      <div className="col-md-4">
+                        <input
+                          type="text"
+                          className={`form-control form-control-md ${errors.description ? "is-invalid" : ""
+                            }`}
+                          placeholder="Enter Description"
+                          value={description}
+                          onChange={(e) => {
+                            setDescription(e.target.value);
+                            if (errors.description) {
+                              setErrors({ ...errors, description: "" });
+                            }
+                          }}
+                        />
+                        {errors.description && (
+                          <div className="invalid-feedback">
+                            {errors.description}
+                          </div>
+                        )}
+                      </div>
+                    </div>
+
+                    <div className="form-group row mt-3">
+                      <label className="col-form-label col-md-2">
+                        Link <span className="text-danger">*</span>
+                      </label>
+                      <div className="col-md-4">
+                        <input
+                          type="text"
+                          className={`form-control form-control-md ${errors.link ? "is-invalid" : ""
+                            }`}
+                          placeholder="Enter Link"
+                          value={link}
+                          onChange={(e) => {
+                            setLink(e.target.value);
+                            if (errors.link) {
+                              setErrors({ ...errors, link: "" });
+                            }
+                          }}
+                        />
+                        {errors.link && (
+                          <div className="invalid-feedback">
+                            {errors.link}
+                          </div>
+                        )}
+                      </div>
+                    </div>
+                    <div className="form-group row">
+                      <label className="col-form-label col-md-2">
+                        Issue Date <span className="text-danger">*</span>
+                      </label>
+                      <div className="cal-icon col-md-4">
+                        <Flatpickr
+                          id="startDatePicker"
+                          className={`form-control ${errors.issueDate ? "is-invalid" : ""
+                            }`}
+                          placeholder="Select Issue Date"
+                          value={issueDate}
+                          onChange={(date) => {
+                            setIssueDate(date[0]);
+                            if (issueDate) {
+                              setErrors({ ...errors, issueDate: "" });
+                            }
+                          }}
+                          options={{
+                            dateFormat: "d-m-Y",
+                            monthSelectorType: "dropdown",
+                            prevArrow:
+                              '<svg><path d="M10 5L5 10L10 15"></path></svg>',
+                            nextArrow:
+                              '<svg><path d="M5 5L10 10L5 15"></path></svg>',
+                          }}
+                        />
+                        {errors.issueDate && (
+                          <small className="invalid-feedback">
+                            {errors.issueDate}
+                          </small>
+                        )}
+                      </div>
+                    </div>
+                    <input
+                      type="submit"
+                      className="btn btn-primary btn-sm"
+                      value="Submit"
+                    />
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddEmpInfo;
