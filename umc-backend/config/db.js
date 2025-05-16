a1cf60c3b0d2b48146fbf60c034f6244ce3c8e27
anii002 <aniket102002@gmail.com>
2025-05-16 11:50:45 +0530

    changes by aniket

--- umc-backend/config/db.js
+++ umc-backend/config/db.js
@@ -1,19 +1,19 @@
 const mysql = require('mysql');
 require('dotenv').config();
 
-// const db = mysql.createConnection({
-//     host: "127.0.0.1",
-//     user: "umc_DB",
-//     password: "^U,*4^cQR(BU",
-//     database: "umc_DB"
-// });
-
 const db = mysql.createConnection({
     host: "127.0.0.1",
-    user: "root",
-    password: "",
+    user: "umc_DB",
+    password: "^U,*4^cQR(BU",
     database: "umc_DB"
 });
+
+// const db = mysql.createConnection({
+//     host: "127.0.0.1",
+//     user: "root",
+//     password: "",
+//     database: "umc_DB"
+// });
 
 db.connect((err) => {
     if (err) throw err;
