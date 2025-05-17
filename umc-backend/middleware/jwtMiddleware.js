3e8a78b83f6bf8077b434bde9baf2e454151156f
Itdevelopment002 <itdevelopment111@gmail.com>
2025-05-17 19:09:03 +0530

    Update token status code
    Abhimanyu

--- umc-backend/middleware/jwtMiddleware.js
+++ umc-backend/middleware/jwtMiddleware.js
@@ -33,7 +33,7 @@ const verifyToken = async (req, res, next) => {
 
   // Verify token
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return res.status(403).json({ message: 'Invalid token' });
+    if (err) return res.status(401).json({ message: 'Invalid token' });
     req.user = user;
     next();
   });
