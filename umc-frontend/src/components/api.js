4619903f9a9ce24d7874f0dd8c21b4fd30313de8
Itdevelopment002 <itdevelopment111@gmail.com>
2025-06-10 03:14:26 -0400

    chnges

--- umc-frontend/src/components/api.js
+++ umc-frontend/src/components/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const baseURL = process.env.REACT_APP_API_BASE_URL;
+export const baseURL = 'https://umc.gov.in/api/api';
 
 const api = axios.create({
   baseURL,
